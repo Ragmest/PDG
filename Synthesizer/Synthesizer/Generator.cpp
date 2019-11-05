9fa77bc7b1d62a6105434a3bd9fa59dfb984af6d
Leoneedus <marmaz.966@o2.pl>
2019-11-05 16:29:05 +0100

    asynch to sync

--- Synthesizer/Synthesizer/Generator.cpp
+++ Synthesizer/Synthesizer/Generator.cpp
@@ -12,9 +12,9 @@ std::vector<float> Generator::generateWhiteNoise(float time, float amplitude, ui
 	std::random_device rd;
 	std::mt19937 gen(rd());
 	float half_amplitude = amplitude / 2.0f;
-	std::uniform_real_distribution<double> dis(-half_amplitude, half_amplitude);
+	std::uniform_real_distribution<float> dis(-half_amplitude, half_amplitude);
 	uint32_t size = static_cast<uint32_t>(static_cast<float>(frequnecy)* time);
 	std::vector<float> result(size);
-	std::generate(result.begin(), result.end(), [&dis, &gen]() {return static_cast<float>(dis(gen)); });
+	std::generate(result.begin(), result.end(), [&dis, &gen]() {return dis(gen); });
 	return result;
 }
--- Synthesizer/Synthesizer/Speaker.cpp
+++ Synthesizer/Synthesizer/Speaker.cpp
@@ -1,60 +1,31 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "Speaker.h"
+
+#include <thread>
 #include <stdio.h>
 #include <math.h>
-#include <thread>
-#include <iostream>
-#include <random>
 #include "portaudio.h"
 
 namespace {
-	void terminate(const PaError& err)
+	constexpr uint32_t SAMPLE_RATE(44100);
+	constexpr uint32_t FRAMES_PER_BUFFER(1024);
+	constexpr float M_PI(3.14159265f);
+	constexpr uint32_t TABLE_SIZE(200u);
+}
+
+void Terminate(const PaError& err)
+{
+	fprintf(stderr, "An error occured while using the portaudio stream\n");
+	fprintf(stderr, "Error number: %d\n", err);
+	fprintf(stderr, "Error message: %s\n", Pa_GetErrorText(err));
+	// Print more information about the error.
+	if (err == paUnanticipatedHostError)
 	{
-		Pa_Terminate();
-		fprintf(stderr, "An error occured while using the portaudio stream\n");
-		fprintf(stderr, "Error number: %d\n", err);
-		fprintf(stderr, "Error message: %s\n", Pa_GetErrorText(err));
+		const PaHostErrorInfo* hostErrorInfo = Pa_GetLastHostErrorInfo();
+		fprintf(stderr, "Host API error = #%ld, hostApiType = %d\n", hostErrorInfo->errorCode, hostErrorInfo->hostApiType);
+		fprintf(stderr, "Host API error = %s\n", hostErrorInfo->errorText);
 	}
-}
-
-
-#define NUM_SECONDS   (5)
-#define SAMPLE_RATE   (44100)
-#define FRAMES_PER_BUFFER  (64)
-
-#ifndef M_PI
-#define M_PI  (3.14159265)
-#endif
-#define TABLE_SIZE   (64)
-typedef struct
-{
-	std::mt19937 gen;
-	std::uniform_real_distribution<float> dis;
-}
-paTestData;
-
-static int patestCallback(const void*, void* outputBuffer,
-	unsigned long framesPerBuffer,
-	const PaStreamCallbackTimeInfo*,
-	PaStreamCallbackFlags,
-	void* userData)
-{
-	paTestData* data = (paTestData*)userData;
-	float* out = (float*)outputBuffer;
-	unsigned long i;
-
-	for (i = 0; i < framesPerBuffer; i++)
-	{
-		float sample = data->dis(data->gen);
-		*out++ = sample;  /* left */
-		*out++ = sample;  /* right */
-	}
-
-	return paContinue;
-}
-
-static void StreamFinished(void*)
-{
+	Pa_Terminate();
 }
 
 void Speaker::play()
@@ -62,28 +33,35 @@ void Speaker::play()
 	PaStreamParameters outputParameters;
 	PaStream* stream;
 	PaError err;
-	int i;
-	std::random_device rd;
-	float half_amplitude = 1.0f / 2.0f;
-
-	paTestData data{
-		std::mt19937(rd()),
-		std::uniform_real_distribution<float>(-half_amplitude, half_amplitude)
-	};
+	float buffer[FRAMES_PER_BUFFER][2]; /* stereo output buffer */
+	float sine[TABLE_SIZE]; /* sine wavetable */
+	int left_phase = 0;
+	int right_phase = 0;
+	int left_inc = 1;
+	int right_inc = 3; /* higher pitch so we can distinguish left and right. */
+	int i, j, k;
+	int bufferCount;
 
 	printf("PortAudio Test: output sine wave. SR = %d, BufSize = %d\n", SAMPLE_RATE, FRAMES_PER_BUFFER);
 
+	/* initialise sinusoidal wavetable */
+	for (i = 0; i < TABLE_SIZE; i++)
+	{
+		sine[i] = (float)sin(((double)i / (double)TABLE_SIZE) * M_PI * 2.);
+	}
+
+
 	err = Pa_Initialize();
-	if (err != paNoError) terminate(err);
+	if (err != paNoError) Terminate(err);
 
 	outputParameters.device = Pa_GetDefaultOutputDevice(); /* default output device */
 	if (outputParameters.device == paNoDevice) {
 		fprintf(stderr, "Error: No default output device.\n");
-		terminate(err);
+		Terminate(err);
 	}
 	outputParameters.channelCount = 2;       /* stereo output */
 	outputParameters.sampleFormat = paFloat32; /* 32 bit floating point output */
-	outputParameters.suggestedLatency = Pa_GetDeviceInfo(outputParameters.device)->defaultLowOutputLatency;
+	outputParameters.suggestedLatency = 0.050; // Pa_GetDeviceInfo( outputParameters.device )->defaultLowOutputLatency;
 	outputParameters.hostApiSpecificStreamInfo = NULL;
 
 	err = Pa_OpenStream(
@@ -93,24 +71,38 @@ void Speaker::play()
 		SAMPLE_RATE,
 		FRAMES_PER_BUFFER,
 		paClipOff,      /* we won't output out of range samples so don't bother clipping them */
-		patestCallback,
-		&data);
-	if (err != paNoError) terminate(err);
+		NULL, /* no callback, use blocking API */
+		NULL); /* no callback, so no callback userData */
+	if (err != paNoError) Terminate(err);
 
-	err = Pa_SetStreamFinishedCallback(stream, &StreamFinished);
-	if (err != paNoError) terminate(err);
+	printf("Play 3 times, higher each time.\n");
 
 	err = Pa_StartStream(stream);
-	if (err != paNoError) terminate(err);
+	if (err != paNoError) Terminate(err);
 
-	std::thread stoppingThread([]() { std::cin.get(); });
-	stoppingThread.join();
+	while (true)
+	{
+		for (j = 0; j < FRAMES_PER_BUFFER; j++)
+		{
+			buffer[j][0] = sine[left_phase];  /* left */
+			buffer[j][1] = sine[right_phase];  /* right */
+			left_phase += 3;
+			if (left_phase >= TABLE_SIZE) left_phase -= TABLE_SIZE;
+			right_phase += 3;
+			if (right_phase >= TABLE_SIZE) right_phase -= TABLE_SIZE;
+		}
+
+		err = Pa_WriteStream(stream, buffer, FRAMES_PER_BUFFER);
+		if (err != paNoError) Terminate(err);
+	}
 
 	err = Pa_StopStream(stream);
-	if (err != paNoError) terminate(err);
+	if (err != paNoError) Terminate(err);
+
+	Pa_Sleep(1000);
 
 	err = Pa_CloseStream(stream);
-	if (err != paNoError) terminate(err);
+	if (err != paNoError) Terminate(err);
 
 	Pa_Terminate();
 	printf("Test finished.\n");
