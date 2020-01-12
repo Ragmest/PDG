d5ea459e1258ca68ca358e13e7eb18691eebf91e
Leoneedus <marmaz.966@o2.pl>
2020-01-12 17:29:51 +0100

    Made helicopter sound generated in real time

---
+++ Audio Scene/Assets/Helicopter/HelicopterSound.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class HelicopterSound : MonoBehaviour
+{
+    AudioSource source;
+
+    [Header("Stearing signal")]
+    [Range(0.0f, 360.0f)]
+    public float phaseShift = 0;
+    public int steeringFrequency = 0;
+    private SteeringSignal steeringSignal;
+
+    [Header("Clip info")]
+    public int sampleRate = 44100;
+    public int duration = 1;
+    public int channels = 2;
+    [Range(0.0f, 1.0f)]
+    public float amplitude = 1.0f;
+
+    System.Random rng = new System.Random();
+
+    private void Start()
+    {
+        steeringSignal = new SteeringSignal(sampleRate, steeringFrequency, phaseShift);
+        AudioClip buffer = AudioClip.Create("white noise", sampleRate * duration, channels, sampleRate, true, generateWhiteNoise);
+
+        source = GetComponent<AudioSource>();
+        if (source != null)
+        {
+            source.clip = buffer;
+            source.loop = true;
+            source.Play();
+        }
+    }
+
+    void generateWhiteNoise(float[] data)
+    {
+        for (int i = 0; i < data.Length; i++)
+            data[i] = (float)((rng.NextDouble() * 2.0) - 1.0) * amplitude;
+
+        steeringSignal.ApplyFilter(data);
+    }
+}
---
+++ Audio Scene/Assets/Scripts/SteeringSignal.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class SteeringSignal
+{
+    private float globalTime = 0.0f;
+    private float deltaTime;
+    private int frequency;
+    private float phaseShift;
+
+    public SteeringSignal(float sampleRate, int frequency, float phaseShift = 0.0f)
+    {
+        deltaTime = 1.0f / sampleRate;
+        this.phaseShift = (phaseShift * Mathf.PI / 180.0f) / frequency;
+        this.frequency = frequency;
+    }
+
+    private float Apply(float inValue)
+    {
+        float outValue = inValue * Mathf.Sin(globalTime * frequency * 2.0f * Mathf.PI + phaseShift);
+        globalTime += deltaTime;
+        return outValue;
+    }
+
+    public void ApplyFilter(float[] inData)
+    {
+        for (int i = 0; i < inData.Length; i++)
+        {
+            inData[i] = Apply(inData[i]);
+        }
+    }
+}
